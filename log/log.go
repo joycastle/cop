dc0403f2f3cc4fb813e99be1ec8d180ff8ee8ab4
LevinLFX <levin@joycastle.mobi>
2022-05-23 17:19:04 +0800

    done

--- log/log.go
+++ log/log.go
@@ -4,7 +4,10 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"sync"
 	"time"
+
+	"github.com/joycastle/cop/util"
 )
 
 const (
@@ -16,6 +19,18 @@ const (
 	magenta = "\033[97;45m"
 	cyan    = "\033[97;46m"
 	reset   = "\033[0m"
+)
+
+const (
+	level_debug = iota
+	level_info
+	level_warn
+	level_fatal
+)
+
+const (
+	Log_Path_Stdout = "!#%WDCXFT97"
+	Log_Path_Stderr = "^&$IJHVZSC8"
 )
 
 var (
@@ -37,86 +52,110 @@ var (
 
 type Logger struct {
 	Logger *log.Logger
-	fpath  string
 	Fptr   *os.File
 	Fname  string
-}
 
-func DisableColor() {
-	color_debug_using = color_close_debug
-	color_info_using = color_close_info
-	color_warn_using = color_close_warn
-	color_fatal_using = color_close_fatal
-}
-
-func EnableColor() {
-	color_debug_using = color_debug
-	color_info_using = color_info
-	color_warn_using = color_warn
-	color_fatal_using = color_fatal
-}
-
-func NewLoggerDefault() *Logger {
-	l := &Logger{
-		fpath: "",
-		Fptr:  os.Stdout,
-		Fname: "",
-	}
-
-	l.Logger = log.New(os.Stdout, "", log.Ldate|log.Lmicroseconds|log.Lshortfile)
-
-	return l
+	fpath      string
+	colorMap   map[int]string
+	mu         *sync.RWMutex
+	expireDays int64
+	oldFiles   map[int64]string
+	oMu        *sync.Mutex
 }
 
 func NewLogger(path string) *Logger {
 	l := &Logger{
-		fpath: path,
+		fpath:      path,
+		colorMap:   make(map[int]string),
+		mu:         new(sync.RWMutex),
+		expireDays: 30,
+		oldFiles:   make(map[int64]string),
+		oMu:        new(sync.Mutex),
 	}
 
-	l.Logger = log.New(os.Stdout, "", log.Ldate|log.Lmicroseconds|log.Lshortfile)
-	l.setup_file()
+	l.EnableColor()
+
+	if path == Log_Path_Stdout {
+		l.Logger = log.New(os.Stdout, "", log.Ldate|log.Lmicroseconds|log.Lshortfile)
+	} else if path == Log_Path_Stderr {
+		l.Logger = log.New(os.Stderr, "", log.Ldate|log.Lmicroseconds|log.Lshortfile)
+	} else {
+		l.Logger = log.New(os.Stderr, "", log.Ldate|log.Lmicroseconds|log.Lshortfile)
+		l.setup_file()
+		l.delete_file()
+	}
 
 	return l
 }
 
-func (l *Logger) Printf(f string, v ...any) {
-	l.Logger.Output(2, color_info_using+fmt.Sprintf(f, v...))
+func (l *Logger) SetExpireDays(days int64) {
+	l.expireDays = days
 }
 
-func (l *Logger) Println(v ...any) {
-	l.Logger.Output(2, color_info_using+fmt.Sprintln(v...))
+func (l *Logger) EnableColor() *Logger {
+	l.mu.Lock()
+	defer l.mu.Unlock()
+	l.colorMap[level_debug] = fmt.Sprintf("[%sDEBUG%s] ", blue, reset)
+	l.colorMap[level_info] = fmt.Sprintf("[%sINFO%s] ", green, reset)
+	l.colorMap[level_warn] = fmt.Sprintf("[%sWARN%s] ", red, reset)
+	l.colorMap[level_fatal] = fmt.Sprintf("[%sFATAL%s] ", yellow, reset)
+	return l
+}
+
+func (l *Logger) DisableColor() *Logger {
+	l.mu.Lock()
+	defer l.mu.Unlock()
+	l.colorMap[level_debug] = "[DEBUG] "
+	l.colorMap[level_info] = "[INFO] "
+	l.colorMap[level_warn] = "[WARN] "
+	l.colorMap[level_fatal] = "[FATAL] "
+	return l
+}
+
+func (l *Logger) pf(level int, f string, v ...any) {
+	l.mu.RLock()
+	color_prefix := l.colorMap[level]
+	l.mu.RUnlock()
+	l.Logger.Output(2, color_prefix+fmt.Sprintf(f, v...))
+}
+
+func (l *Logger) pln(level int, v ...any) {
+	l.mu.RLock()
+	color_prefix := l.colorMap[level]
+	l.mu.RUnlock()
+	l.Logger.Output(2, color_prefix+fmt.Sprintln(v...))
 }
 
 func (l *Logger) Debugf(f string, v ...any) {
-	l.Logger.Output(2, color_debug_using+fmt.Sprintf(f, v...))
+	l.pf(level_debug, f, v...)
 }
 
 func (l *Logger) Debug(v ...any) {
-	l.Logger.Output(2, color_debug_using+fmt.Sprintln(v...))
+	l.pln(level_debug, v...)
 }
 
 func (l *Logger) Infof(f string, v ...any) {
-	l.Logger.Output(2, color_info_using+fmt.Sprintf(f, v...))
+	l.pf(level_info, f, v...)
 }
 
 func (l *Logger) Info(v ...any) {
-	l.Logger.Output(2, color_info_using+fmt.Sprintln(v...))
+	l.pln(level_info, v...)
 }
 
 func (l *Logger) Warnf(f string, v ...any) {
-	l.Logger.Output(2, color_warn_using+fmt.Sprintf(f, v...))
+	l.pf(level_warn, f, v...)
 }
 
 func (l *Logger) Warn(v ...any) {
-	l.Logger.Output(2, color_warn_using+fmt.Sprintln(v...))
+	l.pln(level_warn, v...)
 }
 
 func (l *Logger) Fatalf(f string, v ...any) {
-	l.Logger.Output(2, color_fatal_using+fmt.Sprintf(f, v...))
+	l.pf(level_fatal, f, v...)
 }
 
 func (l *Logger) Fatal(v ...any) {
-	l.Logger.Output(2, color_fatal_using+fmt.Sprintln(v...))
+	l.pln(level_fatal, v...)
 }
 
 func (l *Logger) setup_file() {
@@ -140,10 +179,46 @@ func (l *Logger) setup_file() {
 	l.Fname = fname
 	l.Logger.SetOutput(fp)
 
+	createTime := time.Now().Unix()
+	l.oMu.Lock()
+	l.oldFiles[createTime] = fname
+	l.oMu.Unlock()
+
 	go func() {
 		select {
 		case <-time.After(deadline.Sub(time.Now())):
 			l.setup_file()
+		}
+	}()
+}
+
+func (l *Logger) delete_file() {
+	go func() {
+		for {
+			time.Sleep(time.Second * 3600)
+
+			if l.expireDays <= 0 {
+				continue
+			}
+
+			exTime := time.Now().Unix() - l.expireDays*86400
+
+			var ndel []string
+
+			l.oMu.Lock()
+			for createTime, fileName := range l.oldFiles {
+				if createTime <= exTime {
+					ndel = append(ndel, fileName)
+					delete(l.oldFiles, createTime)
+				}
+			}
+			l.oMu.Unlock()
+
+			if len(ndel) > 0 {
+				for _, fileName := range ndel {
+					util.DeleteFile(fileName)
+				}
+			}
 		}
 	}()
 }
