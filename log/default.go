30736f283e4020da6293f7fe80105a12a34a8d76
LevinLFX <levin@joycastle.mobi>
2022-05-23 17:33:38 +0800

    done

--- log/default.go
+++ log/default.go
@@ -1,7 +1,29 @@
 package log
 
+// default log
 var Default *Logger = NewLogger(Log_Path_Stdout)
+
+// error log
+var Error *Logger = NewLogger(Log_Path_Stderr)
+
+// run log
+var Run *Logger = Default
+
+// monitor log
+var Monitor *Logger = Default
 
 func SetDefault(l *Logger) {
 	Default = l
+}
+
+func SetError(l *Logger) {
+	Error = l
+}
+
+func SetRun(l *Logger) {
+	Run = l
+}
+
+func SetMonitor(l *Logger) {
+	Run = l
 }
